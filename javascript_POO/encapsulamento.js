c9e9ed1faa297469c007e4f00c665d504706d343
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-06-01 10:19:08 -0300

    Create encapsulamento.js

---
+++ javascript_POO/encapsulamento.js
@@ -0,0 +1,15 @@
+class Carro {
+    constructor(){
+        this.modelo = "gol"
+        this.cor = "azul"
+    }
+
+    frear(){
+
+        console.log("parar o carro")
+
+    }
+}
+
+const carro = new Carro()
+carro.frear()
